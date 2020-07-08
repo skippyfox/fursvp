d32d366a67bbc25a1e2637b1f51c0c2ca387dd48
skippyfox <where.is.skippy@gmail.com>
2020-07-07 21:55:50 -0400

    Added react js successfully (as part of fursvp.api for now)

--- src/fursvp.api/Startup.cs
+++ src/fursvp.api/Startup.cs
@@ -15,6 +15,7 @@ namespace Fursvp.Api
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Infrastructure;
     using Microsoft.AspNetCore.Mvc.Routing;
+    using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
@@ -49,6 +50,12 @@ namespace Fursvp.Api
         {
             services.AddCors();
             services.AddControllers();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
 
             services.AddDomainServices();
             services.AddFursvpDataWithFirestore();
@@ -111,8 +118,15 @@ namespace Fursvp.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
             app.UseRouting();
 
@@ -126,6 +140,16 @@ namespace Fursvp.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
             });
         }
     }
