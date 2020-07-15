1be3eaf226ec99bf270b097177443c2e467a0683
skippyfox <where.is.skippy@gmail.com>
2020-07-15 01:51:12 -0400

    Started to add Home page update when logged in/out

--- src/fursvp.api/ClientApp/src/store/FursvpEvents.js
+++ src/fursvp.api/ClientApp/src/store/FursvpEvents.js
@@ -1,5 +1,17 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var UserStore_1 = require("./UserStore");
 // ----------------
 // ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
 // They don't directly mutate state, but they can have external side-effects (such as loading data).
@@ -8,18 +20,35 @@ exports.actionCreators = {
         // Only load data if it's something we don't already have (and are not already loading)
         var appState = getState();
         if (appState && appState.fursvpEvents) {
-            fetch("api/event")
+            var userEmail = UserStore_1.getStoredVerifiedEmail();
+            var authToken = UserStore_1.getStoredAuthToken();
+            var requestOptions = undefined;
+            if (authToken !== undefined) {
+                requestOptions = {
+                    method: 'GET',
+                    credentials: "include",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + authToken
+                    }
+                };
+            }
+            fetch("api/event", requestOptions)
                 .then(function (response) { return response.json(); })
                 .then(function (data) {
                 dispatch({ type: 'RECEIVE_FURSVP_EVENTS', events: data });
             });
-            dispatch({ type: 'REQUEST_FURSVP_EVENTS' });
+            dispatch({ type: 'REQUEST_FURSVP_EVENTS', requestedAsUser: userEmail });
         }
     }; }
 };
 // ----------------
 // REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
-var unloadedState = { events: [], isLoading: false };
+var unloadedState = {
+    events: [],
+    isLoading: false,
+    requestedAsUser: undefined
+};
 exports.reducer = function (state, incomingAction) {
     if (state === undefined) {
         return unloadedState;
@@ -27,15 +56,11 @@ exports.reducer = function (state, incomingAction) {
     var action = incomingAction;
     switch (action.type) {
         case 'REQUEST_FURSVP_EVENTS':
-            return {
-                events: state.events,
-                isLoading: true
-            };
+            return __assign(__assign({}, state), { isLoading: true, requestedAsUser: action.requestedAsUser });
         case 'RECEIVE_FURSVP_EVENTS':
-            return {
-                events: action.events,
-                isLoading: false
-            };
+            return __assign(__assign({}, state), { events: action.events, isLoading: false });
+        case 'USER_LOGGED_OUT_ACTION':
+            return __assign(__assign({}, state), { events: [], isLoading: true });
         default:
             return state;
     }
--- src/fursvp.api/ClientApp/src/store/FursvpEvents.ts
+++ src/fursvp.api/ClientApp/src/store/FursvpEvents.ts
@@ -1,5 +1,6 @@
 ﻿import { Action, Reducer } from 'redux';
 import { AppThunkAction } from '.';
+import { getStoredVerifiedEmail, getStoredAuthToken, UserLoggedInAction, UserLoggedOutAction, OpenLoginModalAction } from './UserStore';
 
 // -----------------
 // STATE - This defines the type of data maintained in the Redux store.
@@ -7,6 +8,7 @@ import { AppThunkAction } from '.';
 export interface FursvpEventsState {
     isLoading: boolean;
     events: FursvpEvent[];
+    requestedAsUser: string | undefined;
 }
 
 export interface FursvpEvent {
@@ -61,6 +63,7 @@ export interface FormResponses {
 
 interface RequestFursvpEventsAction {
     type: 'REQUEST_FURSVP_EVENTS';
+    requestedAsUser: string | undefined;
 }
 
 interface ReceiveFursvpEventsAction {
@@ -70,7 +73,8 @@ interface ReceiveFursvpEventsAction {
 
 // Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
 // declared type strings (and not any other arbitrary string).
-type KnownAction = RequestFursvpEventsAction | ReceiveFursvpEventsAction;
+type KnownAction = RequestFursvpEventsAction | ReceiveFursvpEventsAction
+    | UserLoggedOutAction | UserLoggedInAction | OpenLoginModalAction;
 
 // ----------------
 // ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
@@ -80,14 +84,31 @@ export const actionCreators = {
     requestFursvpEvents: (): AppThunkAction<KnownAction> => (dispatch, getState) => {
         // Only load data if it's something we don't already have (and are not already loading)
         const appState = getState();
+
         if (appState && appState.fursvpEvents) {
-            fetch(`api/event`)
+
+            var userEmail = getStoredVerifiedEmail();
+            var authToken = getStoredAuthToken();
+
+            var requestOptions: RequestInit | undefined = undefined;
+            if (authToken !== undefined) {
+                requestOptions = {
+                    method: 'GET',
+                    credentials: "include",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + authToken
+                    }
+                };
+            }                
+
+            fetch(`api/event`, requestOptions)
                 .then(response => response.json() as Promise<FursvpEvent[]>)
                 .then(data => {
                     dispatch({ type: 'RECEIVE_FURSVP_EVENTS', events: data });
                 });
 
-            dispatch({ type: 'REQUEST_FURSVP_EVENTS' });
+            dispatch({ type: 'REQUEST_FURSVP_EVENTS', requestedAsUser: userEmail });
         }
     }
 };
@@ -95,7 +116,11 @@ export const actionCreators = {
 // ----------------
 // REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
 
-const unloadedState: FursvpEventsState = { events: [], isLoading: false };
+const unloadedState: FursvpEventsState = {
+    events: [],
+    isLoading: false,
+    requestedAsUser: undefined
+};
 
 export const reducer: Reducer<FursvpEventsState> = (state: FursvpEventsState | undefined, incomingAction: Action): FursvpEventsState => {
     if (state === undefined) {
@@ -106,14 +131,22 @@ export const reducer: Reducer<FursvpEventsState> = (state: FursvpEventsState | u
     switch (action.type) {
         case 'REQUEST_FURSVP_EVENTS':
             return {
-                events: state.events,
-                isLoading: true
+                ...state,
+                isLoading: true,
+                requestedAsUser: action.requestedAsUser
             };
         case 'RECEIVE_FURSVP_EVENTS':
             return {
+                ...state,
                 events: action.events,
                 isLoading: false
             };
+        case 'USER_LOGGED_OUT_ACTION':
+            return {
+                ...state,
+                events: [],
+                isLoading: true
+            }
         default:
             return state;
     }
